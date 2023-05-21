30e65feeb544916b71c7dcea41c5e39371afad78
Steven Le <64396816+StevenLe2012@users.noreply.github.com>
2023-05-20 23:13:07 -0700

    react

---
+++ react/nextgen/tests/App.test.ts
@@ -0,0 +1,14 @@
+// import { test, expect } from '@playwright/test';
+
+// test('HiMom component', async ({ page }) => {
+//   await page.goto('http://localhost:3000'); // Replace with the URL where your React app is running
+
+//   const hiMomText = await page.$('.text');
+//   expect(hiMomText).toHaveText('Hi mom');
+
+//   const toggleButton = await page.$('.button');
+//   await toggleButton?.click();
+
+//   const hiddenHiMomText = await page.$('.text');
+//   expect(hiddenHiMomText).toBeNull();
+// });
